b6bd9a08778057c6ea6cb5dab62f2795ddf69417
adr <adrhc@outlook.com>
2019-05-08 20:53:23 +0300

    init

---
+++ src/main/java/com/example/mvctries/MvctriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvctries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvctriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvctriesApplication.class, args);
+	}
+
+}

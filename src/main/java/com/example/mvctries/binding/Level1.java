19f4c94bbe66e7e9a6f628c1305d14e362dc63d6
adr <adrhc@outlook.com>
2019-05-08 21:06:40 +0300

    server.port=8082

--- src/main/java/com/example/mvctries/binding/Level1.java
+++ src/main/java/com/example/mvctries/binding/Level1.java
@@ -6,5 +6,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class Level1 {
+	private int nr1;
 	private Level2 level2;
 }
--- src/main/java/com/example/mvctries/binding/TryController.java
+++ src/main/java/com/example/mvctries/binding/TryController.java
@@ -2,17 +2,17 @@ package com.example.mvctries.binding;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tryctrl")
 @Slf4j
 public class TryController {
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseStatus(HttpStatus.OK)
-	public void post(@RequestBody Level1 level1) {
+	public Level1 post(@RequestBody Level1 level1) {
 		log.debug("level1:\n{}", level1);
+		return level1;
 	}
 }

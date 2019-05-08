033fca2ffb3849d3dbbf0fe9f9bdf906c04ff4d1
adr <adrhc@outlook.com>
2019-05-08 21:10:54 +0300

    Level1 implements Serializable

--- src/main/java/com/example/mvctries/binding/Level1.java
+++ src/main/java/com/example/mvctries/binding/Level1.java
@@ -3,9 +3,11 @@ package com.example.mvctries.binding;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
-public class Level1 {
+public class Level1 implements Serializable {
 	private int nr1;
 	private Level2 level2;
 }
--- src/main/java/com/example/mvctries/binding/Level2.java
+++ src/main/java/com/example/mvctries/binding/Level2.java
@@ -3,8 +3,10 @@ package com.example.mvctries.binding;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
-public class Level2 {
+public class Level2 implements Serializable {
 	private int nr2;
 }

94088c25a74aacac36098dcfb5d9ff2e392f9ad3
adr <adrhc@outlook.com>
2019-05-11 02:31:33 +0300

    refactor

--- src/main/java/com/example/mvctries/controller/TryController.java
+++ src/main/java/com/example/mvctries/controller/TryController.java
@@ -1,8 +1,6 @@
 package com.example.mvctries.controller;
 
-import com.example.mvctries.web.aop.BindingHandler;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.validation.BindingResult;
@@ -17,9 +15,6 @@ import javax.validation.Valid;
 @RequestMapping("/tryctrl")
 @Slf4j
 public class TryController {
-	@Autowired
-	private BindingHandler bindingHandler;
-
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseStatus(HttpStatus.OK)
 	public Level1 post(@Valid @RequestBody Level1 level1, BindingResult errors) {
